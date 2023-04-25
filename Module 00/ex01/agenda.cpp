d83645b848e126cea5a628b8fd0053c657cc4699
Xabier Matute Rusinol <xmatute-@c2r2s5.42urduliz.com>
2023-04-25 19:02:00 +0200

    commit general

--- Module 00/ex01/agenda.cpp
+++ Module 00/ex01/agenda.cpp
@@ -5,54 +5,59 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/16 13:11:49 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/04/20 13:47:54 by xmatute-         ###   ########.fr       */
+/*   Created: 2023/04/23 13:43:05 by xmatute-          #+#    #+#             */
+/*   Updated: 2023/04/25 18:33:18 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PhoneBook/PhoneBook.hpp"
-#include <iostream>
-#include <string>
+#include "agenda.hpp"
 
-static	std::string	ask_comand()
+static void	show_comands()
+{
+	std::cout << "\nMi extraordinaria agenda\n\n";
+	std::cout << "Introduzca uno de los siguientes comandos:\n";
+	std::cout << ADD << ":ADD\n";
+	std::cout << SEARCH << ":SEARCH\n";
+	std::cout << EXIT << ":EXIT\n";
+	std::cout << "\n> ";
+}
+
+static int	ask_comand()
 {
 	std::string comando;
 
-	std::cout << "\nMi extraordinaria agenda\n";
 	if (!std::cin.good())
 	{
 		std::cerr << "❗️Error: algo no anda bien con la entrada estandar\n";
 		exit(1);
 	}
+	show_comands();
 	std::getline(std::cin, comando);
-	if (comando == "ADD" || comando == "SEARCH" || comando == "EXIT")
-		return (comando);
+	if (comando == "ADD" || comando[0] - '0' == ADD)
+		return (ADD);
+	if (comando == "SEARCH" || comando[0] - '0' == SEARCH)
+		return (SEARCH);
+	if (comando == "EXIT" || comando[0] - '0' == EXIT)
+		return (EXIT);
 	std::cout << "Error: comando " << '"' + comando + '"' << " no aceptado\n";
 	std::cout << "Comandos aceptados:\n";
 	std::cout << "\tADD: Agrega un nuevo contacto\n";
 	std::cout << "\tSEARCH: Muestra un contacto especifico\n";
 	std::cout << "\tEXIT\n";
-	return (comando);
+	return (ask_comand());
 }
 
 int main(void)
 {
 	PhoneBook	agenda;
-	std::string	comando;
+	int			comando;
 
-	Contact	c;
-	agenda.add(c.autofill());
-	agenda.add(c);
-	agenda.add(c);
-	agenda.add(c);
-	agenda.add(c);
-	agenda.add(c);
 	comando = ask_comand();
-	while (comando != "EXIT")
+	while (comando != EXIT)
 	{
-		if (comando == "ADD")
+		if (comando == ADD)
 			agenda.add();
-		if (comando == "SEARCH")
+		if (comando == SEARCH)
 			agenda.search();
 		comando = ask_comand();
 	}
