d913727dc093397da1ccffd7201674f98ac1ceef
Xabier Matute Rusinol <xmatute-@c3r7s1.42urduliz.com>
2023-05-07 12:24:23 +0200

    mains

---
+++ Module 01/ex00/v1/main.cpp
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 10:46:57 by xmatute-          #+#    #+#             */
+/*   Updated: 2023/04/27 11:36:35 by xmatute-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+#include <iostream>
+
+void	test0()
+{
+	std::cout << "\nTEST 0\n";
+	Zombie	nameless;
+	Zombie	zero("Zero");
+}
+
+void	test1()
+{
+	std::cout << "\nTEST 1\n";
+	Zombie	nameless;
+	Zombie	zero("Asier");
+
+	nameless.announce();
+	zero.announce();
+}
+
+
+void	test2()
+{
+	std::cout << "\nTEST 2\n";
+	Zombie	*zero = Zombie::newZombie("ChowChow");
+
+	zero->announce();
+	delete(zero);
+}
+
+void	test2c()
+{
+	std::cout << "\nTEST 2 con crash\n";
+	Zombie	*nameless = Zombie::newZombie(NULL);
+	Zombie	*zero = Zombie::newZombie("ChowChow");
+
+	nameless->announce();
+	zero->announce();
+	delete(zero);
+	delete(nameless);
+}
+
+void	test3()
+{
+	std::cout << "\nTEST 3\n";
+	Zombie::randomChump("Aitana");
+	Zombie::randomChump("Zoilo");
+}
+
+int main(void)
+{
+	test0();
+	test1();
+	test2();
+	// test2c();
+	test3();
+	return 0;
+}
