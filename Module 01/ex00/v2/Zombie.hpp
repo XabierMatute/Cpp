47c88223745b3734c57ae66549b85a81523ea387
Xabier Matute Rusinol <xmatute-@c3r7s1.42urduliz.com>
2023-05-07 12:23:02 +0200

    commit general

---
+++ Module 01/ex00/v2/Zombie.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/27 09:50:13 by xmatute-          #+#    #+#             */
+/*   Updated: 2023/04/27 12:00:31 by xmatute-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+
+class	Zombie
+{
+	private:
+		std::string name;
+	
+	public:
+		void	announce( void );
+
+		static Zombie*	newZombie( std::string name );
+		static void		randomChump( std::string name );
+
+		~Zombie();
+		
+	private:
+		Zombie();
+		Zombie( std::string name );
+};
+#endif
