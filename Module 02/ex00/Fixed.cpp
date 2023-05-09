088c8e75c50cbfccaa240aa26ceea91fbf748d9c
Xabier Matute Rusinol <xmatute-@c3r2s1.42urduliz.com>
2023-05-09 19:49:34 +0200

    commit general de Fixed

--- Module 02/ex00/Fixed.cpp
+++ Module 02/ex00/Fixed.cpp
@@ -6,46 +6,57 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 16:37:35 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/05/09 19:00:24 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:42:49 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
+// Definición de macros para colores
+#define RESET   "\033[0m"
+#define BLACK   "\033[0;30m"
+#define RED     "\033[0;31m"
+#define GREEN   "\033[0;32m"
+#define YELLOW  "\033[0;33m"
+#define BLUE    "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define CYAN    "\033[0;36m"
+#define WHITE   "\033[0;37m"
+#define GRAY    "\033[0;90m"
 
 Fixed::Fixed() 
 {
-	std::cout << "Default constructor called" << std::endl;
+	std::cout << BLUE << "🔧Default constructor called" << RESET << std::endl;
 	value = 0;
 }
 
 Fixed::~Fixed() 
 {
-	std::cout << "Destructor called" << std::endl;
+	std::cout << RED << "💣Destructor called" << RESET << std::endl;
 }
 
 Fixed::Fixed(const Fixed& to_copy) 
 {
-	std::cout << "Copy constructor called" << std::endl;
+	std::cout << BLUE << "🛠 Copy constructor called" << RESET << std::endl;
 	*this = to_copy;
 }
 
 Fixed& Fixed::operator=(const Fixed& to_asign)
 {
-	std::cout << "Assignation operator called" << std::endl;
+	std::cout << YELLOW << "👆Assignation operator called" << RESET << std::endl;
 	value = to_asign.getRawBits();
 	return *this;
 }
 
 int Fixed::getRawBits( void ) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	std::cout << GRAY << "🔍getRawBits member function called" << RESET << std::endl;
 	return value;
 }
 
 void Fixed::setRawBits ( int const raw )
 {
-	std::cout << "setRawBits member function called" << std::endl;
+	std::cout << GRAY << "✏️setRawBits member function called" << RESET <<  std::endl;
 	value = raw;
 }
--- Module 02/ex00/main.cpp
+++ Module 02/ex00/main.cpp
@@ -6,15 +6,51 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 18:23:16 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/05/09 18:47:18 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:49:07 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
 
+void	test0()
+{
+	std::cout << "\nTest 0" << std::endl;
+	Fixed zero;
+	std::cout << "hola jonh comanche" << std::endl;
+	
+}
+
+void	test1()
+{
+	std::cout << "\nTest 1" << std::endl;
+	Fixed numero;
+	std::cout << numero.getRawBits() << std::endl;
+	numero.setRawBits(42);
+	std::cout << numero.getRawBits() << std::endl;
+}
+
+void	test2()
+{
+	std::cout << "\nTest 2" << std::endl;
+	Fixed numero;
+	std::cout << numero.getRawBits() << std::endl;
+	numero.setRawBits(42);
+	std::cout << numero.getRawBits() << std::endl;
+	Fixed otro(numero);
+	std::cout << otro.getRawBits() << std::endl;
+	Fixed	zero;
+	std::cout << zero.getRawBits() << std::endl;
+	numero = zero;
+	std::cout << numero.getRawBits() << std::endl;
+	std::cout << (zero = otro).getRawBits() << std::endl;
+	std::cout << zero.getRawBits() << std::endl;
+}
+
 void	subjecttest()
 {
+	std::cout << "\nTest subject" << std::endl;
+
 	Fixed a;
 	Fixed b( a );
 	Fixed c;
@@ -26,6 +62,9 @@ void	subjecttest()
 
 int main()
 {
+	test0();
+	test1();
+	test2();
 	subjecttest();
 	return 0;
 }
