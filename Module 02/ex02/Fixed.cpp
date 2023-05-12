7c459f0d0d51f36fb0cef07ce2c1a7dbc7e372f1
Xabier Matute Rusinol <xmatute-@c3r3s6.42urduliz.com>
2023-05-12 16:31:26 +0200

    mensajes descomentados

--- Module 02/ex02/Fixed.cpp
+++ Module 02/ex02/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 16:37:35 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/05/11 16:29:05 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/05/12 16:30:23 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,13 +48,13 @@ static int ipow(int a, int n)
 
 Fixed::Fixed() 
 {
-	// std::cout << BLUE << "🔧Default constructor called" << RESET << std::endl;
+	std::cout << BLUE << "🔧Default constructor called" << RESET << std::endl;
 	value = 0;
 }
 
 Fixed::~Fixed() 
 {
-	// std::cout << RED << "💣Destructor called" << RESET << std::endl;
+	std::cout << RED << "💣Destructor called" << RESET << std::endl;
 }
 
 Fixed::Fixed(const Fixed& to_copy) 
@@ -65,7 +65,7 @@ Fixed::Fixed(const Fixed& to_copy)
 
 Fixed::Fixed(const int& to_asign) 
 {
-	// std::cout << BLUE << "🔢Int constructor called" << RESET << std::endl;
+	std::cout << BLUE << "🔢Int constructor called" << RESET << std::endl;
 	value = to_asign * pow(2, bit_num);
 }
 
