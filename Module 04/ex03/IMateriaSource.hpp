128496a3ffc77bc4501cdbebdc61a1818b8d8576
Xabier Matute Rusinol <xmatute-@c3r5s1.42urduliz.com>
2023-05-31 18:40:03 +0200

    commit general de Materia

---
+++ Module 04/ex03/IMateriaSource.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/31 13:15:15 by xmatute-          #+#    #+#             */
+/*   Updated: 2023/05/31 17:10:09 by xmatute-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
+
+#include "AMateria.hpp"
+
+class IMateriaSource
+{
+	public:
+	virtual ~IMateriaSource() {}
+	virtual void learnMateria(AMateria*) = 0;
+	virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
+#endif
